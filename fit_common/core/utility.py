9f3aa7afb57bb8d68e0ce8c151e85c7f94392ba0
Fabio Zito <zitelog@gmail.com>
2025-04-16 17:07:44 +0200

    workaround get_version

--- fit_common/core/utility.py
+++ fit_common/core/utility.py
@@ -240,11 +240,7 @@ def is_cmd(name):
 
 
 def get_version():
-    parser = ConfigParser()
-    parser.read(resolve_path("assets/config.ini"))
-    version = parser.get("fit_properties", "tag_name")
-
-    return version
+    return "v0.0.0"
 
 
 def get_logo():
