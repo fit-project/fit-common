44a5c6d98756bab0f0eeca4bd5369d880b4d6232
Fabio Zito <zitelog@gmail.com>
2026-01-27 10:59:33 +0100

    feat(paths): allow optional filename for db and log paths

--- fit_common/core/paths.py
+++ fit_common/core/paths.py
@@ -47,7 +47,7 @@ def resolve_app_path(subfolder: str = "FIT") -> str:
     return full_path
 
 
-def resolve_db_path(filename: str) -> str:
+def resolve_db_path(filename: str | None = None) -> str:
     db_dir = os.path.join(resolve_app_path(), "data")
     os.makedirs(db_dir, exist_ok=True)
     if not filename:
@@ -55,7 +55,7 @@ def resolve_db_path(filename: str) -> str:
     return os.path.join(db_dir, filename)
 
 
-def resolve_log_path(filename: str) -> str:
+def resolve_log_path(filename: str | None = None) -> str:
     log_dir = os.path.join(resolve_app_path(), "logs")
     os.makedirs(log_dir, exist_ok=True)
     if not filename:
